9138e0500f24ec1fdc526372b33186e0be154318
oekintaro <tarokinoe@gmail.com>
2025-08-09 20:15:59 +0200

    add gtest

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <cmath>
+#include <gtest/gtest.h>
 
+using namespace std;
 
-int main(){
-    std::cout << "Hello, world!\n";
-    return 0;
+TEST(HelloTest, BasicAssertions) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
+
+int main() {
 }
